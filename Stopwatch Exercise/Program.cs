b768a46d213b07f05fade2cec64adc66e4db329e
hamish galpin <hamishg2009@hotmail.co.uk>
2020-07-13 20:02:10 +0100

    Add project files.

---
+++ Stopwatch Exercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Timers;
+
+namespace Stopwatch_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is a exercise for a Stopwatch application." +
+                             "\nTo start it please type 'Start' & Press the Enter Key while running to stop the application");
+            var stopwatch = new Stopwatch();
+            
+
+            
+
+            while (true)
+            {
+                try
+                {
+                    var input = Console.ReadLine();
+
+                    switch (input.ToLower())
+                    {
+                        case "start":
+                        stopwatch.Start();
+                            stopwatch.SetTimer();
+                        break;
+                        default: 
+                            stopwatch.Stop();
+                            Console.WriteLine(stopwatch.Duration); 
+                            stopwatch.ATimer.Stop();
+                            stopwatch.ATimer.Dispose();
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Stopwatch Exercise/Stopwatch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using System.Timers;
+
+namespace Stopwatch_Exercise
+{
+    public class Stopwatch
+    {
+        private DateTime _startTime = DateTime.UtcNow;
+        public System.Timers.Timer ATimer;
+
+        public TimeSpan Duration { get; private set; }
+        public bool IsRunning { get; private set; }
+
+        public void Start()
+        {
+            if (IsRunning)
+
+                throw new InvalidOperationException("the stopwatch has already started running.");
+
+            _startTime = DateTime.UtcNow; 
+            IsRunning = true;
+        }
+
+        public void Stop()
+        {
+            if (!IsRunning)
+                throw new InvalidOperationException("the stopwatch has not been started");
+
+            IsRunning = false;
+            Duration = DateTime.UtcNow - _startTime;
+        }
+
+        public  void SetTimer()
+        {
+            ATimer = new System.Timers.Timer(400);
+            ATimer.Elapsed += OnTimedEvent;
+            ATimer.AutoReset = true;
+            ATimer.Enabled = true;
+
+        } 
+        public void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Console.Clear();
+            Console.WriteLine("time Elapsed {0:mm:ss}",e.SignalTime);
+        }
+
+    }
+
+}
